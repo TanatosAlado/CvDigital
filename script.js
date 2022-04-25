ad0d6cf7edcd2006a95b04db1b2ff2abc1c8086c
Cristian Alessandria <cristian_alessandria@hotmail.com>
2022-04-25 14:01:27 -0300

    Inicio CV

---
+++ script.js
@@ -0,0 +1,14 @@
+var items = document.querySelectorAll(".timeline li");
+ 
+// code for the isElementInViewport function
+ 
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
